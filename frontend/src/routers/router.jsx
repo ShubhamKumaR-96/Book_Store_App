89ea24f61bb5678d746ef6a4d115803821b947e2
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2024-10-15 19:55:04 +0530

    Basic setup completed

---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,28 @@
+import {createBrowserRouter} from "react-router-dom"
+import App from "../App";
+
+const router=createBrowserRouter([
+    {
+    path:'/',
+    element: <App />,
+    children:[
+        {
+            path:'/',
+            element:<div>Home</div>
+        },
+        {
+            path:'/about',
+            element:<div> About</div>
+        },
+        {
+            path:'/orders',
+            element:<div> Orders</div>
+        },
+        {
+            path:'/contact',
+            element:<div> Contact</div>
+        }
+    ]
+    }
+])
+export default router;
